94b674db956706faf147f68201d14f1758651594
umar1990 <ismi1990@gmail.com>
2016-05-23 17:00:44 +0530

    Add files via upload

--- springSec/authorizer/src/main/java/com/demo/authorizer/controllers/AuthorizerController.java
+++ springSec/authorizer/src/main/java/com/demo/authorizer/controllers/AuthorizerController.java
@@ -1,10 +1,13 @@
 package com.demo.authorizer.controllers;
 
+import java.security.Principal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +17,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.demo.authorizer.dvo.EventInDVO;
 import com.demo.authorizer.dvo.EventOutDVO;
+import com.demo.authorizer.dvo.TaskDetailsDVO;
+import com.demo.authorizer.dvo.TaskEstimationSheetDVO;
+import com.demo.authorizer.dvo.TaskInfoDVO;
+import com.demo.authorizer.dvo.TaskSheetDVO;
 
 @Controller
 public class AuthorizerController {
@@ -33,6 +40,30 @@ public class AuthorizerController {
 		return "teamresources";
 	}
 	
+	@RequestMapping("/tasksheet")
+	public ModelAndView viewTaskSheet(){
+		TaskDetailsDVO taskDetailsDVO = new TaskDetailsDVO();
+		List<TaskDetailsDVO> taskDetailsDVOs = new ArrayList<TaskDetailsDVO>();
+		taskDetailsDVOs.add(taskDetailsDVO);
+		TaskSheetDVO taskSheet = new TaskSheetDVO();
+		taskSheet.setTaskDetailsDVOs(taskDetailsDVOs);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		taskSheet.setSheetDate(dateFormat.format(new Date()));
+		ModelAndView mv = new  ModelAndView("tasksheet", "taskSheet", taskSheet);
+		return mv;
+	}
+	
+	@RequestMapping("/taskestimation")
+	public ModelAndView viewTaskEstimationSheet(){
+		TaskInfoDVO taskInfoDVO = new TaskInfoDVO();
+		List<TaskInfoDVO> taskInfoDVOs = new ArrayList<TaskInfoDVO>();
+		taskInfoDVOs.add(taskInfoDVO);
+		TaskEstimationSheetDVO taskEstSheet = new TaskEstimationSheetDVO();
+		taskEstSheet.setTaskInfoDVOs(taskInfoDVOs);;
+		ModelAndView mv = new  ModelAndView("taskestimation", "taskEstSheet", taskEstSheet);
+		return mv;
+	}
+	
 	@RequestMapping("/editprofile")
 	public String editProfile(){
 		return "editprofile";
@@ -45,11 +76,11 @@ public class AuthorizerController {
 		model.setViewName("security/login");
 		if (error != null) {
 			model.addObject("loginMsg", "Invalid username and password!");
-			model.addObject("loginClass", "alert-danger");
+			model.addObject("loginClass", "red");
 		}
 		if (logout != null) {
 			model.addObject("loginMsg", "Hasta la vista!!! Logged out successfully");
-			model.addObject("loginClass", "alert-success");
+			model.addObject("loginClass", "positive");
 		}
 		return model;
 
@@ -65,5 +96,15 @@ public class AuthorizerController {
 		event.setEnd(new Date());
 		events.add(event);
 		return events;
+	}
+	
+	@RequestMapping("saveTaskSheet")
+	public ModelAndView saveTaskSheet(@ModelAttribute TaskSheetDVO taskSheet, Principal principal){
+		System.out.println(principal.getName());
+		System.out.println(taskSheet);
+		ModelAndView mv = new ModelAndView("tasksheet", "taskSheet",taskSheet);
+		mv.addObject("disabled", "disabled");
+		mv.addObject("saveStatus", "Your task has been successfully saved");
+		return mv;
 	}
 }
